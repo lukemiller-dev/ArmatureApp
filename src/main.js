0493c8ee3b9b803ab6ea3c18d7590502932c0e86
Luke Miller <luke.miller1101@gmail.com>
2017-03-21 16:25:31 -0400

    removed bootstrap download with cdn

--- src/main.js
+++ src/main.js
@@ -10,8 +10,6 @@ Promise.config({
 export function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
-     .developmentLogging()
-    .plugin('aurelia-bootstrap')
     .feature('resources');
 
   if (environment.debug) {
